58233a946142e91500a1bf9532ddd15ffbb5981b
Bek McD <bekahmcdevitt@gmail.com>
2019-04-27 16:14:23 -0400

    updated with initial code from example

--- api/connection.js
+++ api/connection.js
@@ -1,3 +1,18 @@
-import { connection } from "mongoose";
+/* handles connection to mongodb and sets name of database to be used*/ 
+const mongoose = require('mongoose');
 
-connection.js file 
+const connectionString = "mongodb://localhost/ FILE PATH?"; //find out how to select file extension and where came from on example code
+
+/* this happened automatically- find out what it is before deleting 
+import { connection } from "mongoose"; */
+
+//newUrlParser disables deprication warning (taken from example code, not sure if needed at this point, will debug)
+mongoose.connect(connectionString, { useNewUrlParser: true})
+    .then(() => {
+        console.log("connected to mongo at: " + connectionString);
+    });
+
+module.exports = mongoose;
+
+
+
