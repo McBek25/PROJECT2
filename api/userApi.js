0f7d6f0578b08c6560664ab26f7baa40f3852a52
Bek McD <bekahmcdevitt@gmail.com>
2019-04-27 16:50:16 -0400

    took out some notation

--- api/userApi.js
+++ api/userApi.js
@@ -2,15 +2,13 @@ const mongoose = require('./connection.js');
 
 //defines shape of entities 
 const UserSchema = mongoose.Schema({
-    name: String //commas on all but last, believe
+    name: String //commas on all but last, believe is correct syntax
 });
 
 //create API communicate with "users" collection in mongodb
 let UserCollection = mongoose.model("User", UserSchema);
 
-/*functions 
 
-here*/
 
 //use mongoose's api to get all user documents and returns promis with all user documents
 function get AllUsers() {
@@ -26,7 +24,6 @@ function getUserById(userId) {
 }
 
 module.exports = {
-    //function names, comma on all but last
     getAllUsers,
     createNewUser,
     getUserById
