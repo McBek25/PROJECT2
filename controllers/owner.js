41510592c2145571c5d55f8a501a8a9c753b0532
Bek McD <bekahmcdevitt@gmail.com>
2019-05-01 18:46:58 -0400

    fixed typo

--- controllers/owner.js
+++ controllers/owner.js
@@ -2,3 +2,11 @@ const allergyApi = require('../api/allergyApi.js');
 const recipeApi = require('../api/recipeApi.js');
 const userApi = require('../api/userApi.js');
 
+const ownerView = function(req, res) {
+    
+}
+
+const ownerController = {
+
+}
+
