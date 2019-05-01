bb6f860fb82673c97c9ba58b7b2d20405aa715a9
Bek McD <bekahmcdevitt@gmail.com>
2019-05-01 12:20:47 -0400

    update so that user pulls all allergy options vs all users

--- api/userApi.js
+++ api/userApi.js
@@ -17,14 +17,21 @@ const UserSchema = mongoose.Schema({
 });
 
 //create API communicate with "users" collection in mongodb
-let UserCollection = mongoose.model("User", UserSchema);
+//let UserCollection = mongoose.model("User", UserSchema);
 
+//need to change all users feature to all allergies for full crud?  bc single user does not need access to all users
+
+//generates all allergy options for user to select
+let AllergyCollection = mongoose.model('Allergy', allergySchema);
+getAllAllergies() {
+    return AllergyCollection.find();
+}
 
 
 //use mongoose's api to get all user documents and returns promis with all user documents
-function getAllUsers() {
+/*function getAllUsers() {
     return UserCollection.find();
-}
+}*/
 
 function createNewUser(newUserData) {
     return UserCollection.create(newUserData);
