d5c52a403bc21b56faeaf4c69aeb328c4e5a0f91
Bek McD <bekahmcdevitt@gmail.com>
2019-05-01 13:42:38 -0400

    crud and exports for recipes

--- api/recipeApi.js
+++ api/recipeApi.js
@@ -15,3 +15,35 @@ const recipesSchema = mongoose.Schema({
     instructions: [string],
 });
 
+
+const RecipeCollection = require('../model/Recipe.js');
+
+
+//CREATE new recipe (this will be owner only and designated in controller file)
+function createNewRecipe(newRecipe) {
+    return RecipeCollection.create(newRecipe);
+}
+
+//READ recipes
+function listRecipes(allRecipe) {
+    return RecipeCollection.get(newRecipe);
+}
+
+//UPDATE recipe (owner only, designated in controller file)
+function updateRecipe(recipeId) {
+    return RecipeCollection.put(recipeId);
+}
+//patching vs putting in a list of selected recipes
+
+//DELETE recipe (owner only, designated on controller file)
+function deleteRecipe(recipeId) {
+    return RecipeCollection.delete(recipeId);
+}
+
+module.exports = {
+    createNewRecipe,
+    listRecipes,
+    updateRecipe,
+    deleteRecipe,
+};
+
